c762c397cba2650c1c5eea106a4cf0b61f5577f0
TI Sabbir <tisabbir0@gmail.com>
2024-03-21 21:28:14 +0600

    go back button added and handled

--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,13 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
   const post = useLoaderData();
   const { id, title, body } = post;
+
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
   return (
     <div>
       <h4>Post No : {id} </h4>
       <h2>Title : {title}</h2>
       <p>{body}</p>
+      <button onClick={handleGoBack}>Go Back</button>
     </div>
   );
 };
